d552ef111000ed3cb0b94373175ee92859d28763
adarsh946 <adarsh.shukla2724@gmail.com>
2025-08-28 11:59:07 +0530

    added middleware

---
+++ backend/src/middlewares/middleware.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
+interface customJwtPayload extends JwtPayload {
+  userId: string;
+}
+
+const useMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const header = req.headers["authorization"];
+  const token = header?.split(" ")[1];
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Token not found",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET!
+    ) as customJwtPayload;
+    if (!decoded) {
+      return res.status(401).json({
+        message: "Invalid Token",
+      });
+    }
+
+    req.userId = decoded.userId;
+    next();
+  } catch (error) {
+    return res.status(401).json({
+      message: "Invalid Token",
+    });
+  }
+};
+
+export default useMiddleware;
